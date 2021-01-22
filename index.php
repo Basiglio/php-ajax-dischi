5b918cc1f851a6c76d6725aa3f2215be8bfab674
Emilio <emilio.furnari87@gmail.com>
2021-01-22 22:10:05 +0100

    finito

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php  
 include __DIR__."/db.php";
 // var_dump($albums);
+
 ?>
 
 
@@ -12,16 +13,26 @@ include __DIR__."/db.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/app.css">
-    <title>Document</title>
+    <title>PHP DISCHI</title>
 
 </head>
 <body>
     <div class="wrapper_90">
     <header>
-        <img src="" alt="">
+        <img src="img/spotify_toplogo.png" alt="">
     </header>
     <main>
-        
+        <div class="albums_container">
+            <?php foreach ($albums as $album ) {  
+                echo "<div class='album'>"."<div class='cover'>".
+                "<img src='".$album['poster']."'"."alt=''>".
+                "</div>".
+                "<h3>".$album['title']."</h3>".
+                "<h3>".$album['author']."</h3>".
+                "<h4>".$album['year']."</h4>".
+                "</div>";
+            } ?> 
+        </div>
     </main>
     </div>
 </body>
